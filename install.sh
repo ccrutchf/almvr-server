287184ffa24c36cda963e0396ad2ede925a017a8
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-05-30 20:07:21 -0700

    -y

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 wget -q packages-microsoft-prod.deb https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 
-sudo apt-get install apt-transport-https
+sudo apt-get install apt-transport-https -y
 sudo apt-get update
-sudo apt-get install dotnet-sdk-2.1
+sudo apt-get install dotnet-sdk-2.1 -y
