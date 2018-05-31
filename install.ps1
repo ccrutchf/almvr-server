0dfbcc7e6c2248d6f8e5bfff9359a6cf49b10820
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-05-30 20:06:01 -0700

    install script

--- install.ps1
+++
@@ -1,5 +0,0 @@
-# Install dotnetcore sdk.
-& choco install dotnetcore-sdk --version 2.1.300-rc1 --source https://www.myget.org/F/alm-vr/api/v2 -y --no-progress
-
-# Force Unity version.
-& choco install unity --version 2018.1.0 -y --no-progress
