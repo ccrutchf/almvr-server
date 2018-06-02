bc9d678bb85a2b5edf14e7fdbfe2214ab5b93f78
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-06-02 15:58:13 -0700

    skip host checking

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 echo $SSH_PRIVATE_KEY > ~/.ssh/id_rsa
 echo $SSH_PUBLIC_KEY > ~/.ssh/id_rsa.pub
 
-ssh root@$SERVER_IP 'bash -c' < ./remote_deploy.sh
+ssh -o StrictHostKeyChecking=no root@$SERVER_IP 'bash -c' < ./remote_deploy.sh
