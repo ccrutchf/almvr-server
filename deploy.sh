cad22384a35b5d661b099b246c73f6fcf1a0d867
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-06-02 16:15:38 -0700

    chmod private key

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 echo $SSH_PRIVATE_KEY > ~/.ssh/id_rsa
 echo $SSH_PUBLIC_KEY > ~/.ssh/id_rsa.pub
 
+chmod 0600 ~/.ssh/id_rsa
+
 ssh -o StrictHostKeyChecking=no root@$SERVER_IP 'bash -c' < ./remote_deploy.sh
