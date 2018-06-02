972af132c96fb1bd30d709bfcca155c6e513442c
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-06-02 16:25:51 -0700

    specify ssh key

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ echo $SSH_PUBLIC_KEY > ~/.ssh/id_rsa.pub
 
 chmod 0600 ~/.ssh/id_rsa
 
-ssh -o StrictHostKeyChecking=no root@$SERVER_IP 'bash -c' < ./remote_deploy.sh
+ssh -vvv -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no root@$SERVER_IP 'bash -c' < ./remote_deploy.sh
