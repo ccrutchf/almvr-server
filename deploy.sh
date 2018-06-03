6ff8ac7d0bc2329b2bce0527b3cd71c2dad57222
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-06-02 17:03:28 -0700

    use sshpass

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ echo $SSH_PUBLIC_KEY > ~/.ssh/id_rsa.pub
 
 chmod 0600 ~/.ssh/id_rsa
 
-ssh -o StrictHostKeyChecking=no root@$SERVER_IP 'bash -c' < ./remote_deploy.sh
+sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_IP 'bash -c' < ./remote_deploy.sh
--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 wget -q packages-microsoft-prod.deb https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 
-sudo apt-get install apt-transport-https -y
+sudo apt-get install apt-transport-https sshpass -y
 sudo apt-get update
 sudo apt-get install dotnet-sdk-2.1 -y
