89086e8d25b981c8c5317bb8b7461645a5a3be8c
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-06-02 19:08:41 -0700

    remove old docker container

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -2,5 +2,8 @@
 /snap/bin/docker container stop "almvr-server"
 /snap/bin/docker container rm "almvr-server"
 
+# Clean up after ourselves
+/snap/bin/docker image rm $(docker inspect --format='{{.Config.Image}}' "almvr-server")
+
 # Start a new instance
 /snap/bin/docker run -dit --restart unless-stopped -v almvr-data:/data -p 9090:80 --name="almvr-server" ccrutchf/almvr:$DOCKER_VERSION
