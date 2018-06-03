917e55e94dc894c1a145f36b8e1f956f2d094160
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-06-02 18:41:09 -0700

    execute file

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ echo Checking docker file...
 cat ~/docker-version
 
 sshpass -p "$SSH_PASSWORD" scp -o StrictHostKeyChecking=no ./remote_deploy.sh $SSH_USER@$SSH_IP:~/
-sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_IP "export DOCKER_VERSION='$(cat ~/docker-version)'" #" && bash -s" < ./remote_deploy.sh
+sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_IP "export DOCKER_VERSION='$(cat ~/docker-version)' && chmod u+x ./remote_deploy.sh && ./remote_deploy.sh"
