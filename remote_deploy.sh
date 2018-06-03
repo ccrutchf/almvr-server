c6b9e27aa22cc4c8096f8311818d1f2b662ec66c
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-06-02 19:09:35 -0700

    grab docker version before we stop the container

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -1,9 +1,11 @@
+OLD_DOCKER_VERSION=$(docker inspect --format='{{.Config.Image}}' "almvr-server")
+
 # Stop old instances
 /snap/bin/docker container stop "almvr-server"
 /snap/bin/docker container rm "almvr-server"
 
 # Clean up after ourselves
-/snap/bin/docker image rm $(docker inspect --format='{{.Config.Image}}' "almvr-server")
+/snap/bin/docker image rm $OLD_DOCKER_VERSION
 
 # Start a new instance
 /snap/bin/docker run -dit --restart unless-stopped -v almvr-data:/data -p 9090:80 --name="almvr-server" ccrutchf/almvr:$DOCKER_VERSION
