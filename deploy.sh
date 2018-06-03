cae659c8479416b4a325af7c990f7426bec1a0b2
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-06-02 18:02:27 -0700

    try again

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_IP "DOCKER_VERSION=$(cat ~/docker-version) && echo $DOCKER_VERSION && bash -s" < ./remote_deploy.sh
+sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_IP "DOCKER_VERSION=$(cat ~/docker-version) && echo $DOCKER_VERSION > ~/othertest" # && bash -s" < ./remote_deploy.sh
