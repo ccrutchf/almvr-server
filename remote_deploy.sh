85afd9e956f2f6ddbe3e82e79bd8017d1eb1ef57
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-06-02 18:45:02 -0700

    full path to docker

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -1,9 +1,9 @@
 # Stop old instances
-docker container stop "almvr-server"
-docker container rm "almvr-server"
+/snap/bin/docker container stop "almvr-server"
+/snap/bin/docker container rm "almvr-server"
 
 # Start a new instance
-docker run -dit --restart unless-stopped -v almvr-data:/data -p 9090:80 --name="almvr-server" ccrutchf/almvr:$DOCKER_VERSION
+/snap/bin/docker run -dit --restart unless-stopped -v almvr-data:/data -p 9090:80 --name="almvr-server" ccrutchf/almvr:$DOCKER_VERSION
 
 # Clean up unused images
-docker images -q | xargs docker rmi
+/snap/bin/docker images -q | xargs /snap/bin/docker rmi
