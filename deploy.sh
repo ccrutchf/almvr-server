224651bde51f691eabe6b0a51aee610696223fcd
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-06-02 17:17:07 -0700

    script cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,1 @@
-echo $SSH_PRIVATE_KEY > ~/.ssh/id_rsa
-echo $SSH_PUBLIC_KEY > ~/.ssh/id_rsa.pub
-
-chmod 0600 ~/.ssh/id_rsa
-
 sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_IP 'bash -c' < ./remote_deploy.sh
