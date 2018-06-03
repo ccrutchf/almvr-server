52ea490dca97dbb6cc3931c60b4fdba48fbada65
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-06-02 18:06:06 -0700

    more info

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
+echo Checking docker file...
+cat ~/docker-version
+
 sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_IP "DOCKER_VERSION=$(cat ~/docker-version) && echo $DOCKER_VERSION > ~/othertest" # && bash -s" < ./remote_deploy.sh
