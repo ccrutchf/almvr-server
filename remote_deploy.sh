48d2d383cd3773a6ae4b2c3eeaff1ee0de4bde8f
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-06-02 18:57:26 -0700

    reorder script

--- remote_deploy.sh
+++ remote_deploy.sh
@@ -2,8 +2,8 @@
 /snap/bin/docker container stop "almvr-server"
 /snap/bin/docker container rm "almvr-server"
 
-# Start a new instance
-/snap/bin/docker run -dit --restart unless-stopped -v almvr-data:/data -p 9090:80 --name="almvr-server" ccrutchf/almvr:$DOCKER_VERSION
-
 # Clean up unused images
 /snap/bin/docker images -q | xargs /snap/bin/docker rmi
+
+# Start a new instance
+/snap/bin/docker run -dit --restart unless-stopped -v almvr-data:/data -p 9090:80 --name="almvr-server" ccrutchf/almvr:$DOCKER_VERSION
